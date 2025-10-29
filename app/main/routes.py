7f556c4c9e1f2fdabb4036bb8a21fd8b0a7d067a
Luiz Felipe Mantovani da Silva <lfmdstrabalho@gmail.com>
2025-10-29 18:53:37 -0300

    remoção do regex do arquivo routes e exclusão dos flash

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,18 +1,10 @@
 
-from flask import render_template, redirect, url_for, flash, request
-from flask_login import login_user, logout_user, login_required, current_user
+from flask import render_template, redirect, url_for, request
+from flask_login import login_user, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
-import re
 from . import main_bp
 from .models import User
 from ..extensions import db
-
-
-
-regex_username = r"^[A-Za-z]{3,}$" #Restringe apenas letras, com limite mínimo de 3 caracteres
-regex_email = r"^[a-zA-Z0-9._%+-]+@gmail\.com$" #Deve ser um e-mail válido com domínio @gmail.com
-regex_senha = r"^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$" #Restringe senhas com limite de 8 caratacteres, incluindo maiúsculas, minúsculas e números
-
 
 @main_bp.route('/', methods=['GET', 'POST'])
 def login():
@@ -26,8 +18,6 @@ def login():
         if user and check_password_hash(user.password, password):
             login_user(user)
             return redirect(url_for('trilha.telatrilha'))
-        else:
-            flash('Usuário ou senha inválidos!')
     return render_template('index.html')
 
 @main_bp.route('/register', methods=['GET', 'POST'])
@@ -41,33 +31,15 @@ def register():
         password = request.form['password']
         check_password = request.form['check_password']
 
-        # Validação do Nome de Usuário 
-        if not re.match(regex_username, username):
-            flash("O nome de usuário deve conter mínimo 3 caracteres e apenas letras.")
-            return redirect(url_for('main.register'))
-
-        # Validação do E-mail 
-        if not re.match(regex_email, email):
-            flash("O e-mail deve ser válido e ter o domínio @gmail.com.")
-            return redirect(url_for('main.register'))
-
-        # Validação de Formato da Senha 
-        if not re.match(regex_senha, password):
-            flash("Senhas com limite de 8 caracteres, incluindo maiúsculas, minúsculas e números")
-            return redirect(url_for('main.register'))
-
         #Confirmação de Senha
         if password != check_password:
-            flash("As senhas não conferem.")
             return redirect(url_for('main.register'))
 
         # Resto das checagens de usuário e e-mail existente no DB
         if User.query.filter_by(username=username).first():
-            flash("Usuário já existe")
             return redirect(url_for('main.register'))
         
         if User.query.filter_by(email=email).first():
-            flash("E-mail já cadastrado")
             return redirect(url_for('main.register'))
 
         #Criação e salvamento do novo usuário
@@ -76,11 +48,9 @@ def register():
         db.session.add(new_user)
         try:
             db.session.commit()
-            flash("Usuário criado com sucesso! Faça login.")
             return redirect(url_for('main.login'))
         except Exception as e:
             db.session.rollback()
-            flash("Erro ao salvar no banco. Tente novamente mais tarde.")
             print(f"[ERRO DB] {e}")
             return redirect(url_for('main.register'))
     return render_template('register.html')
--- app/main/static/js/info.js
+++ app/main/static/js/info.js
@@ -4,6 +4,10 @@ const password = document.querySelector('#password');//Aqui seleciona o input de
 const toggleCheckPassword = document.querySelector('#toggleCheckPassword');//Aqui seleciona o ícone do olho de checagem de senha pelo id (toggleCheckPassword) da (class="fas fa-eye") definido no html
 const checkPassword = document.querySelector('#checkpassword');//Aqui seleciona o input de senha pelo id (checkpassword) definido no html
 
+const register_form = document.querySelector('#register_form') // id do form register
+
+
+
 //Adiciona uma função, escutador de eventos,  que é executada quando o ícone do olho principal (togglePassword) é clicado.
 togglePassword.addEventListener('click', function () {
     const type = password.type === 'password' ? 'text' : 'password';
@@ -23,3 +27,163 @@ toggleCheckPassword.addEventListener('click', function () {
     this.classList.toggle('fa-eye');//olho normal
     this.classList.toggle('fa-eye-slash');//olho cortado
 });
+
+
+// validações antes de enviar o formulário com js
+
+register_form.addEventListener('submit', function (e) {
+    e.preventDefault() // impede que a pagina recarregue
+    
+    let allValid = true;
+
+    const fields = [
+        {
+            id: 'username',
+            validator: nameIsValid
+        },
+        {
+            id: 'email',
+            validator: emailIsValid
+        },
+        {
+            id: 'password',
+            validator: passwordIsSecure
+        },
+        {
+            id: 'checkpassword',
+            validator: passwordMatch
+        }
+    ]
+
+    const errorIcon = '<i class="fa-solid fa-circle-exclamation"></i>';
+
+    fields.forEach(function (field) {
+        const input = document.getElementById(field.id);
+        const inputGroup = input.closest('.input-group');
+        const inputValue = input.value;       
+        
+        const errorSpan = inputGroup.querySelector('.error');
+        errorSpan.innerHTML = '';
+
+        inputGroup.classList.remove('invalid');
+        inputGroup.classList.add('valid');
+
+        const fieldValidator = field.validator(inputValue);
+        
+        if(!fieldValidator.isValid) {
+            errorSpan.innerHTML = `${errorIcon} ${fieldValidator.errorMessage}`;
+            inputGroup.classList.add('invalid');
+            inputGroup.classList.remove('valid')
+            allValid = false;
+        }
+    })
+    
+    if(allValid) {
+        register_form.submit();
+    }
+
+})
+
+function isEmpty(value) {
+    return value === '';
+}
+
+function nameIsValid(value) {
+    const validator = {
+        isValid: true,
+        errorMessage: null
+    };
+
+    if(isEmpty(value)) {
+        validator.isValid = false;
+        validator.errorMessage = 'O nome é obrigatório!'
+        return validator;
+    }
+
+    const min = 3;
+    if(value.length < min) {
+        validator.isValid = false;
+        validator.errorMessage = `O nome deve ter no mínimo ${min} caracteres!`
+        return validator;
+    }
+
+    const regex = /^[\p{L} ]+$/u;
+    if(!regex.test(value)) {
+        validator.isValid = false;
+        validator.errorMessage = 'O nome deve conter apenas letras!'
+        return validator;
+    }
+
+    return validator
+}
+
+function emailIsValid(value) {
+    const validator = {
+        isValid: true,
+        errorMessage: null
+    };
+
+    if(isEmpty(value)) {
+        validator.isValid = false;
+        validator.errorMessage = 'O email é obrigatório!'
+        return validator;
+    }
+
+    const regex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
+
+    if(!regex.test(value)) {
+        validator.isValid = false;
+        validator.errorMessage = 'O e-mail precisa ser válido!'
+        return validator;
+    }
+
+    return validator;
+}
+
+function passwordIsSecure(value) {
+    const validator = {
+        isValid: true,
+        errorMessage: null
+    }
+
+    if (isEmpty(value)) {
+        validator.isValid = false;
+        validator.errorMessage = 'A senha é obrigatória!';
+        return validator;
+    }
+
+    const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*(),.?":{}|<>])[A-Za-z\d!@#$%^&*(),.?":{}|<>]{8,}$/;
+
+
+    if (!regex.test(value)) {
+        validator.isValid = false;
+        validator.errorMessage = `
+            Sua senha deve conter ao menos: <br/>
+            8 dígitos <br/>
+            1 letra minúscula <br/>
+            1 letra maiúscula  <br/>
+            1 número </br>
+            1 caractere especial!
+        `;
+        return validator;
+    }
+
+    return validator;
+}
+
+function passwordMatch(value) {
+    const validator = {
+        isValid: true,
+        errorMessage: null
+    }
+
+    const passwordValue = document.getElementById('password').value;
+
+    if(value === '' || passwordValue !== value) {
+        validator.isValid = false;
+        validator.errorMessage = 'Senhas não condizem!';
+        return validator;
+    }
+
+    return validator;
+}
