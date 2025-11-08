26223dddd4d02050766eff2160b35f9dbfe763fd
Luiz Felipe Mantovani da Silva <lfmdstrabalho@gmail.com>
2025-11-08 14:42:10 -0300

    retirando porta

--- run.py
+++ run.py
@@ -3,5 +3,5 @@ from app import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(ssl_context=('cert.pem', 'key.pem'), port=5443, debug=True)
+    app.run(ssl_context=('cert.pem', 'key.pem'), debug=True)
     
