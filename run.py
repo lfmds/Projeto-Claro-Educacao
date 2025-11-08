858eeb8062a8c4b5467f7917cf814530c3f511ff
Luiz Felipe Mantovani da Silva <lfmdstrabalho@gmail.com>
2025-11-08 14:45:34 -0300

    retirando porta e colocando ip o.o.o.o

--- run.py
+++ run.py
@@ -3,5 +3,5 @@ from app import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(ssl_context=('cert.pem', 'key.pem'), debug=True)
+    app.run(host='0.0.0.0', ssl_context=('cert.pem', 'key.pem'), debug=True)
     
