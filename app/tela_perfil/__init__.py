ead558a66fe2ed8617109c57f9cbfaec41341dca
Luiz Felipe Mantovani da Silva <lfmdstrabalho@gmail.com>
2025-11-08 13:46:19 -0300

    Foi realizado a inclusão do frontend tela perfil. Está funcional, realizando interações com botões e utilizando os dados UserNmae e Email do banco

--- app/tela_perfil/__init__.py
+++ app/tela_perfil/__init__.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, render_template, redirect, request, url_for
+from flask_login import current_user, logout_user
 from flask_login import current_user
 
-perfil = Blueprint('perfil', __name__, template_folder='templates')
+perfil = Blueprint('perfil', __name__, template_folder='templates', static_folder='static', static_url_path='/perfil/static')
 
 @perfil.before_request
 def require_login():
@@ -11,6 +12,10 @@ def require_login():
 @perfil.route('/perfil', methods=['GET', 'POST'])
 def perfil_page():
     if request.method == "POST":
-        if 'action' in request.form and request.form['action'] == 'back':
-            return redirect(url_for('trilha.telatrilha'))
-    return render_template('perfil.html')
+        # Esta é a lógica que será acionada pelo formulário/botão no HTML
+        if 'action' in request.form and request.form['action'] == 'logout':
+            logout_user()
+            return redirect(url_for('main.login'))
+        
+    # Se a requisição for GET, ou se não for um logout, renderiza a página
+    return render_template('perfil.html', user=current_user) # Certifique-se de que o render_template está aqui
